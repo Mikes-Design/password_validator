fc1bae40d238600d1de5871b426f3fd9010bd7fb
Michael <mikesilkysmooth@gmail.com>
2025-04-08 22:42:20 -0400

    cleaned up more files and finished check_spaces function

--- check_spaces.py
+++ check_spaces.py
@@ -2,17 +2,12 @@
     #Check for spaces in password
     #Return True/False and message
 #1
-def has_space(passwrd):
-#   return has_space in passwrd
-#   if has_space in passwrd:
-#      print (True)
-#   else:
-      print(False)
-           
-#2          
-#def has_space(passwrd):          
-#    return ' ' in passwrd
+def check_spaces(passwrd):
+    if " " in passwrd:
+        print("Password cannot contain spaces")
+        return True
+    else:
+        print("Password does not contain spaces")
+        return False
+    
 
-# 3
-# password = "passwrd"
- #    print(has_space(password))          
